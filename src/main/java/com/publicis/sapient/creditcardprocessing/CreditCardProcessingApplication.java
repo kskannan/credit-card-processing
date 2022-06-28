8f794d00cb1796c6bed288699d783160fd07ca3b
Senthamaraikannan <kannan8505@gmail.com>
2022-06-28 23:06:57 +0100

    initial commit

---
+++ src/main/java/com/publicis/sapient/creditcardprocessing/CreditCardProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.publicis.sapient.creditcardprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardProcessingApplication.class, args);
+	}
+
+}
